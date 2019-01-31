b0c4f42233339eef7be73b748e9895fb2a5eccfe
Matt Harris <yesitsmattharris@gmail.com>
2019-01-31 20:16:15 +0000

    BT Turn tank InputAxis

--- Source/BattleTank/TankMovementComponent.cpp
+++ Source/BattleTank/TankMovementComponent.cpp
@@ -5,19 +5,22 @@
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	// auto Time = GetWorld()->GetSeconds();
-	auto Name = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
-
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 
 	// TODO prevent double-speed from dual control use
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 }
--- Source/BattleTank/TankMovementComponent.h
+++ Source/BattleTank/TankMovementComponent.h
@@ -22,6 +22,8 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
 
 private:
 	UTankTrack* LeftTrack = nullptr;
